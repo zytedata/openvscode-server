904d03e421ffac6664ee8fe1a1a760b8a9da9e18
Eric Amodio <eamodio@microsoft.com>
2020-08-30 00:51:29 -0400

    Adds titleDescription to TreeView

--- src/vs/vscode.proposed.d.ts
+++ src/vs/vscode.proposed.d.ts
@@ -2176,4 +2176,16 @@ declare module 'vscode' {
 		}): Disposable;
 	}
 	//#endregion
+
+	//#region https://github.com/microsoft/vscode/issues/105667
+
+	export interface TreeView<T> {
+		/**
+		 * An optional human-readable description that will be rendered in the title of the view.
+		 * Setting the title description to null, undefined, or empty string will remove the title description from the view.
+		 */
+		titleDescription?: string | undefined;
+	}
+
+	//#endregion
 }
