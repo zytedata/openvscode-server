395993705c9e980ed81f19e309088709dcc55d78
Anton Kosyakov <anton.kosyakov@typefox.io>
2020-09-02 20:29:49 +0000

    gitpod server initial commit

---
+++ extensions/gitpod-shared/portsview/src/App.svelte
@@ -0,0 +1,21 @@
+<!-----------------------------------------------------------------------------------------------
+	Copyright (c) Gitpod. All rights reserved.
+------------------------------------------------------------------------------------------------>
+<script lang="ts">
+	import { vscode } from "./utils/vscodeApi";
+	import PortTable from "./porttable/PortTable.svelte";
+	import type { GitpodPortObject } from "./protocol/gitpod";
+
+	let ports: GitpodPortObject[] = [];
+
+	window.addEventListener("message", (event) => {
+		if (event.data.command === "updatePorts") {
+			ports = event.data.ports;
+		}
+	});
+	vscode.postMessage({ command: "queryPortData" });
+</script>
+
+<main>
+	<PortTable {ports} />
+</main>
