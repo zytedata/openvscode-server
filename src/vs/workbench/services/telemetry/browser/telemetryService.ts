d53bcca959b95b1cfd31bd7b0d646569e6f5c164
Anton Kosyakov <anton.kosyakov@typefox.io>
2020-09-02 20:29:49 +0000

    gitpod server initial commit

--- src/vs/workbench/services/telemetry/browser/telemetryService.ts
+++ src/vs/workbench/services/telemetry/browser/telemetryService.ts
@@ -19,6 +19,8 @@ import { isInternalTelemetry, ITelemetryAppender, NullTelemetryService, supports
 import { IBrowserWorkbenchEnvironmentService } from 'vs/workbench/services/environment/browser/environmentService';
 import { IRemoteAgentService } from 'vs/workbench/services/remote/common/remoteAgentService';
 import { resolveWorkbenchCommonProperties } from 'vs/workbench/services/telemetry/browser/workbenchCommonProperties';
+// eslint-disable-next-line code-import-patterns
+import { GitpodInsightsAppender } from 'vs/gitpod/browser/gitpodInsightsAppender';
 
 export class TelemetryService extends Disposable implements ITelemetryService {
 
@@ -72,6 +74,8 @@ export class TelemetryService extends Disposable implements ITelemetryService {
 			const telemetryProvider: ITelemetryAppender = remoteAgentService.getConnection() !== null ? { log: remoteAgentService.logTelemetry.bind(remoteAgentService), flush: remoteAgentService.flushTelemetry.bind(remoteAgentService) } : new OneDataSystemWebAppender(isInternal, 'monacoworkbench', null, productService.aiConfig?.ariaKey);
 			appenders.push(telemetryProvider);
 			appenders.push(new TelemetryLogAppender(loggerService, environmentService));
+			const gitpodTelemetryProvider: ITelemetryAppender = new GitpodInsightsAppender(productService);
+			appenders.push(gitpodTelemetryProvider);
 			const config: ITelemetryServiceConfig = {
 				appenders,
 				commonProperties: resolveWorkbenchCommonProperties(storageService, productService.commit, productService.version, isInternal, environmentService.remoteAuthority, productService.embedderIdentifier, productService.removeTelemetryMachineId, environmentService.options && environmentService.options.resolveCommonTelemetryProperties),
