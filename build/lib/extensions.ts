bb12405148448e85e84052e0b1a8fefaa2a1ce58
Anton Kosyakov <anton@gitpod.io>
2021-03-04 09:44:07 +0000

    exclude github-authentication extensions

--- build/lib/extensions.ts
+++ build/lib/extensions.ts
@@ -238,6 +238,7 @@ const excludedExtensions = [
 	'ms-vscode.node-debug2',
 	'vscode-notebook-tests',
 	'vscode-custom-editor-tests',
+	'vscode.github-authentication'
 ];
 
 const marketplaceWebExtensionsExclude = new Set([
