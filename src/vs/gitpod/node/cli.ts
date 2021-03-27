a22cf7818a62c32d3d4c8bae161d12d580d93db2
Anton Kosyakov <anton@gitpod.io>
2021-03-27 15:59:01 +0000

    [cli] use last focused window

--- src/vs/gitpod/node/cli.ts
+++ src/vs/gitpod/node/cli.ts
@@ -5,16 +5,13 @@
 import * as assert from 'assert';
 import * as fs from 'fs';
 import * as path from 'path';
-import { streamToBufferReadableStream } from 'vs/base/common/buffer';
 import { URI } from 'vs/base/common/uri';
 import { whenDeleted } from 'vs/base/node/pfs';
-import { IRequestContext } from 'vs/base/parts/request/common/request';
 import { localize } from 'vs/nls';
 import { NativeParsedArgs } from 'vs/platform/environment/common/argv';
 import { buildHelpMessage, buildVersionMessage, ErrorReporter, OptionDescriptions, OPTIONS as ALL_OPTIONS, parseArgs } from 'vs/platform/environment/node/argv';
 import { createWaitMarkerFile } from 'vs/platform/environment/node/wait';
 import product from 'vs/platform/product/common/product';
-import { asJson } from 'vs/platform/request/common/request';
 import type { PipeCommand } from 'vs/workbench/api/node/extHostCLIServer';
 
 const OPTIONS_KEYS: (keyof typeof ALL_OPTIONS)[] = [
@@ -136,43 +133,12 @@ async function sendCommand(command: PipeCommand): Promise<string> {
 		port = Number(process.env.GITPOD_THEIA_PORT);
 	}
 	const http = await import('http');
-	interface LinksResult {
-		links: string[]
-	}
-	const result = await new Promise<LinksResult | null>((resolve, reject) => {
-		const request = http.request({
+	return new Promise<string>((resolve, reject) => {
+		const req = http.request({
 			hostname: 'localhost',
 			port,
 			protocol: 'http:',
-			path: '/gitpod-cli-server-sockets',
-			method: 'GET',
-			timeout: 5000
-		}, response => {
-			if (response.statusCode !== 200) {
-				reject(new Error(`failed to fetch cli server sockets: ${response.statusCode} (${response.statusMessage})`));
-				return;
-			}
-			try {
-				resolve(asJson<LinksResult>({
-					res: response, stream: streamToBufferReadableStream(response)
-				} as IRequestContext));
-			} catch (e) {
-				reject(e);
-			}
-		});
-		request.on('error', reject);
-		request.end();
-	});
-	if (!result?.links.length) {
-		throw new Error('please open window');
-	}
-	if (result.links.length > 1) {
-		throw new Error('there should be only one opened window, please close other');
-	}
-	const link = result.links[0];
-	return new Promise<string>((resolve, reject) => {
-		const req = http.request({
-			socketPath: link,
+			path: '/cli',
 			method: 'POST',
 			timeout: 5000
 		}, res => {
