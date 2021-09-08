7e3c8cdb7bc17acdcaf28faffd3a48b15c14460d
Anton Kosyakov <anton@gitpod.io>
2021-09-08 09:20:51 +0000

    enable VS Code Desktop in stable

--- extensions/gitpod-shared/src/features.ts
+++ extensions/gitpod-shared/src/features.ts
@@ -321,29 +321,27 @@ export async function registerWorkspaceCommands(context: GitpodExtensionContext)
 		vscode.env.openExternal(vscode.Uri.parse('https://github.com/gitpod-io/gitpod/issues/new/choose'))
 	));
 
-	if (context.info.getIdeAlias() === 'code-latest') {
-		if (vscode.env.uiKind === vscode.UIKind.Web) {
-			function openDesktop(scheme: 'vscode' | 'vscode-insiders'): void {
-				const uri = vscode.workspace.workspaceFile || vscode.workspace.workspaceFolders?.[0]?.uri;
-				vscode.env.openExternal(vscode.Uri.from({
-					scheme,
-					authority: 'gitpod.gitpod-desktop',
-					path: uri?.path || context.info.getWorkspaceLocationFile() || context.info.getWorkspaceLocationFolder() || context.info.getCheckoutLocation(),
-					query: JSON.stringify({
-						instanceId: context.info.getInstanceId(),
-						workspaceId: context.info.getWorkspaceId(),
-						gitpodHost: context.info.getGitpodHost()
-					})
-				}));
-			}
-			context.subscriptions.push(vscode.commands.registerCommand('gitpod.openInStable', () => openDesktop('vscode')));
-			context.subscriptions.push(vscode.commands.registerCommand('gitpod.openInInsiders', () => openDesktop('vscode-insiders')));
+	if (vscode.env.uiKind === vscode.UIKind.Web) {
+		function openDesktop(scheme: 'vscode' | 'vscode-insiders'): void {
+			const uri = vscode.workspace.workspaceFile || vscode.workspace.workspaceFolders?.[0]?.uri;
+			vscode.env.openExternal(vscode.Uri.from({
+				scheme,
+				authority: 'gitpod.gitpod-desktop',
+				path: uri?.path || context.info.getWorkspaceLocationFile() || context.info.getWorkspaceLocationFolder() || context.info.getCheckoutLocation(),
+				query: JSON.stringify({
+					instanceId: context.info.getInstanceId(),
+					workspaceId: context.info.getWorkspaceId(),
+					gitpodHost: context.info.getGitpodHost()
+				})
+			}));
 		}
-		if (vscode.env.uiKind === vscode.UIKind.Desktop) {
-			context.subscriptions.push(vscode.commands.registerCommand('gitpod.openInBrowser', () =>
-				vscode.env.openExternal(vscode.Uri.parse(context.info.getWorkspaceUrl()))
-			));
-		}
+		context.subscriptions.push(vscode.commands.registerCommand('gitpod.openInStable', () => openDesktop('vscode')));
+		context.subscriptions.push(vscode.commands.registerCommand('gitpod.openInInsiders', () => openDesktop('vscode-insiders')));
+	}
+	if (vscode.env.uiKind === vscode.UIKind.Desktop) {
+		context.subscriptions.push(vscode.commands.registerCommand('gitpod.openInBrowser', () =>
+			vscode.env.openExternal(vscode.Uri.parse(context.info.getWorkspaceUrl()))
+		));
 	}
 
 	const communityStatusBarItem = vscode.window.createStatusBarItem('gitpod.community', vscode.StatusBarAlignment.Right, -100);
