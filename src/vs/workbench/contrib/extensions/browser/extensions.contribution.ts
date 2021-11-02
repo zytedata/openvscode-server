7a2da8c1f27470546d29279eaff5e6194885cd0a
jeanp413 <jeanp413@hotmail.com>
2021-11-02 17:08:54 -0500

    Install extension with version using workbench.extensions.installExtension

--- src/vs/workbench/contrib/extensions/browser/extensions.contribution.ts
+++ src/vs/workbench/contrib/extensions/browser/extensions.contribution.ts
@@ -74,6 +74,7 @@ import { ExtensionsCompletionItemsProvider } from 'vs/workbench/contrib/extensio
 import { IQuickInputService } from 'vs/platform/quickinput/common/quickInput';
 import { Event } from 'vs/base/common/event';
 import { IPaneCompositePartService } from 'vs/workbench/services/panecomposite/browser/panecomposite';
+import { getIdAndVersion } from 'vs/platform/extensionManagement/common/extensionManagementCLIService';
 
 // Singletons
 registerSingleton(IExtensionsWorkbenchService, ExtensionsWorkbenchService);
@@ -294,7 +295,8 @@ CommandsRegistry.registerCommand({
 		const extensionGalleryService = accessor.get(IExtensionGalleryService);
 		try {
 			if (typeof arg === 'string') {
-				const [extension] = await extensionGalleryService.getExtensions([{ id: arg }], CancellationToken.None);
+				const [id, version] = getIdAndVersion(arg);
+				const [extension] = await extensionGalleryService.getExtensions([{ id, version }], CancellationToken.None);
 				if (extension) {
 					const installOptions: InstallOptions = {
 						isMachineScoped: options?.donotSync ? true : undefined, /* do not allow syncing extensions automatically while installing through the command */
