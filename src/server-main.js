cb92e300d5a4c156abf3fdce3c87f9cad2bbf115
Gerard Braad <me@gbraad.nl>
2021-11-10 06:31:12 +0000

    Fixes 219 Provide a consistent default port. This is done to keep the default port consistent before 1.62 release where we started using the upstream server

--- src/server-main.js
+++ src/server-main.js
@@ -171,7 +171,7 @@ function sanitizeStringArg(val) {
  *
  * If only `--port` is provided then connect to that port.
  *
- * In absence of specified ports, connect to port 8000.
+ * In absence of specified ports, connect to port 3000.
  * @param {string | undefined} host
  * @param {string | undefined} strPort
  * @param {string | undefined} strPickPort
@@ -219,7 +219,7 @@ async function parsePort(host, strPort, strPickPort) {
 			process.exit(1);
 		}
 	}
-	return 8000;
+	return 3000;
 }
 
 /**
