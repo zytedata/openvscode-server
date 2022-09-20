6a51197f458f130bc8b1d227bc3e70699f43f940
mustard <mhqnwt@gmail.com>
2022-09-20 10:58:07 +0000

    Add UnhandledError telemetry error report

--- src/vs/gitpod/browser/gitpodInsightsAppender.ts
+++ src/vs/gitpod/browser/gitpodInsightsAppender.ts
@@ -1,19 +1,25 @@
-/* eslint-disable code-import-patterns */
+/* eslint-disable local/code-import-patterns */
 /* eslint-disable header/header */
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Gitpod. All rights reserved.
  *--------------------------------------------------------------------------------------------*/
 
+/// <reference types='@gitpod/gitpod-protocol/lib/typings/globals'/>
+
 import { IProductService } from 'vs/platform/product/common/productService';
 import { ITelemetryAppender } from 'vs/platform/telemetry/common/telemetryUtils';
-import { mapMetrics, mapTelemetryData } from 'vs/gitpod/common/insightsHelper';
+import { mapMetrics, mapTelemetryData, ReportErrorParam } from 'vs/gitpod/common/insightsHelper';
 import type { IDEMetric } from '@gitpod/ide-metrics-api-grpcweb';
+import type { ErrorEvent } from 'vs/platform/telemetry/common/errorTelemetry';
 
 type SendMetrics = (metrics: IDEMetric[]) => Promise<void>;
+type ErrorReports = (errors: ReportErrorParam) => Promise<void>;
+interface SupervisorWorkspaceInfo { gitpodHost: string; instanceId: string; workspaceId: string }
 
 export class GitpodInsightsAppender implements ITelemetryAppender {
 	private readonly _baseProperties: { appName: string; uiKind: 'web'; version: string };
 	private readonly devMode = this.productService.nameShort.endsWith(' Dev');
+	private gitpodUserId: string | undefined;
 	constructor(
 		@IProductService private readonly productService: IProductService
 	) {
@@ -22,11 +28,19 @@ export class GitpodInsightsAppender implements ITelemetryAppender {
 			uiKind: 'web',
 			version: this.productService.version,
 		};
+		window.gitpod?.service.server.getLoggedInUser().then((user) => {
+			this.gitpodUserId = user.id;
+		}).catch((e) => {
+			console.error('failed to get gitpodUserId', e);
+		});
 	}
 
 	public log(eventName: string, data: any): void {
 		this.sendAnalytics(eventName, data);
 		this.sendMetrics(eventName, data);
+		if (eventName === 'UnhandledError') {
+			this.sendErrorReports(data as ErrorEvent);
+		}
 	}
 
 	private sendAnalytics(eventName: string, data: any): void {
@@ -81,29 +95,87 @@ export class GitpodInsightsAppender implements ITelemetryAppender {
 			return this._sendMetrics;
 		}
 		return this._sendMetrics = (async () => {
-			let gitpodHost: string | undefined;
-			if (!this.devMode) {
-				const infoResponse = await fetch(window.location.protocol + '//' + window.location.host + '/_supervisor/v1/info/workspace', {
-					credentials: 'include'
-				});
-				if (!infoResponse.ok) {
-					throw new Error(`Getting workspace info failed: ${infoResponse.statusText}`);
-				}
-				const info: { gitpodHost: string } = await infoResponse.json();
-				gitpodHost = new URL(info.gitpodHost).host;
-			} else if (this.productService.gitpodPreview) {
-				gitpodHost = this.productService.gitpodPreview.host;
-			}
-			if (!gitpodHost) {
+			const gitpodWsInfo = await this.getGitpodWorkspaceInfo();
+			if (!gitpodWsInfo.gitpodHost) {
 				return undefined;
 			}
 			// load grpc-web before see https://github.com/gitpod-io/gitpod/issues/4448
 			await import('@improbable-eng/grpc-web');
 			const { MetricsServiceClient, sendMetrics } = await import('@gitpod/ide-metrics-api-grpcweb');
-			const ideMetricsEndpoint = 'https://ide.' + gitpodHost + '/metrics-api';
+			const ideMetricsEndpoint = 'https://ide.' + gitpodWsInfo.gitpodHost + '/metrics-api';
 			const client = new MetricsServiceClient(ideMetricsEndpoint);
 			return async (metrics: IDEMetric[]) => {
 				await sendMetrics(client, metrics);
+			};
+		})();
+	}
+
+	private async sendErrorReports(error: ErrorEvent) {
+		const gitpodWsInfo = await this.getGitpodWorkspaceInfo();
+		const params: ReportErrorParam = {
+			workspaceId: gitpodWsInfo.workspaceId,
+			instanceId: gitpodWsInfo.instanceId,
+			errorStack: error.callstack,
+			userId: this.gitpodUserId ?? '',
+			component: 'vscode-web',
+			version: this._baseProperties.version,
+			properties: {
+				error_name: error.uncaught_error_name,
+				error_message: error.msg,
+				...this._baseProperties,
+			}
+		};
+		if (this.devMode && this.productService.gitpodPreview?.log?.errorReports) {
+			console.log('Gitpod Error Reports: ', JSON.stringify(params, undefined, 2));
+		}
+		const doSend = await this.getSendErrorReports();
+		if (doSend) {
+			await doSend(params);
+		}
+	}
+
+	private _sendErrorReports: Promise<ErrorReports | undefined> | undefined;
+	private getSendErrorReports(): Promise<ErrorReports | undefined> {
+		if (this._sendErrorReports) {
+			return this._sendErrorReports;
+		}
+		return this._sendErrorReports = (async () => {
+			const gitpodWsInfo = await this.getGitpodWorkspaceInfo();
+			if (!gitpodWsInfo.gitpodHost) {
+				return undefined;
+			}
+			const ideMetricsHttpEndpoint = 'https://ide.' + gitpodWsInfo.gitpodHost + '/metrics-api/reportError';
+			return async (params: ReportErrorParam) => {
+				const response = await fetch(ideMetricsHttpEndpoint, {
+					method: 'POST',
+					body: JSON.stringify(params),
+					credentials: 'omit',
+				});
+				if (!response.ok) {
+					const data = await response.json();
+					console.error(`Cannot report error: ${response.status} ${response.statusText}`, data);
+				}
+			};
+		})();
+	}
+
+	private _gitpodWsInfo: Promise<SupervisorWorkspaceInfo> | undefined;
+	private getGitpodWorkspaceInfo(): Promise<SupervisorWorkspaceInfo> {
+		if (this._gitpodWsInfo) {
+			return this._gitpodWsInfo;
+		}
+		return this._gitpodWsInfo = (async () => {
+			const infoResponse = await fetch(window.location.protocol + '//' + window.location.host + '/_supervisor/v1/info/workspace', {
+				credentials: 'include'
+			});
+			if (!infoResponse.ok) {
+				throw new Error(`Getting workspace info failed: ${infoResponse.statusText}`);
+			}
+			const info: SupervisorWorkspaceInfo = await infoResponse.json();
+			return {
+				gitpodHost: this.devMode ? this.productService.gitpodPreview?.host ?? 'gitpod-staging.com' : new URL(info.gitpodHost).host,
+				instanceId: info.instanceId,
+				workspaceId: info.workspaceId
 			};
 		})();
 	}
